84f2c89f86185aee94abb5122adb74865285a40d
Matheus Wichman <matheushw@outlook.com>
2019-12-23 11:17:32 -0300

    Update ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'superdividendos.com.br',
+    'www.superdividendos.com.br'
+]
 
 USE_X_FORWARDED_HOST = True
 
